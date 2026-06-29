c1742a011f45b7ed797d8ed8a13c9aef807125a1
Demis Balbach <db@minikn.xyz>
2026-06-30 00:09:19 +0200

    host: workhorse: add ssh and gnupg features

--- hosts/workhorse.nix
+++ hosts/workhorse.nix
@@ -5,6 +5,11 @@
     font.enable = true;
     theme.enable = true;
     ghostty.enable = true;
+    ssh.enable = true;
+    gnupg = {
+      enable = true;
+      sshKeys = [ "E3FFA5A1B444A4F099E594758008C1D8845EC7C0" ];
+    };
     zsh = {
       enable = true;
       extraConfig = ''
