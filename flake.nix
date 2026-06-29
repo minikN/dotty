e47902c23895d2007b6ea637f78c185716d6b0a8
Demis Balbach <db@minikn.xyz>
2026-06-30 00:45:50 +0200

    flake: add agenix

--- flake.nix
+++ flake.nix
@@ -15,6 +15,11 @@
     };
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs:
