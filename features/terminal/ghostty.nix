d30b7b7f56abe125e652a01042dd7b28be74fe91
Demis Balbach <db@minikn.xyz>
2026-06-29 13:51:15 +0200

    features: Add ghostty feature

---
+++ features/terminal/ghostty.nix
@@ -0,0 +1,76 @@
+{ lib, mkFeature, ... }:
+
+mkFeature {
+  name = "ghostty";
+
+  options = { pkgs, ... }: {
+    package = lib.mkPackageOption pkgs "ghostty-bin" { };
+  };
+
+  globals = { config, ... }: {
+    apps.terminal = lib.mkIf config.features.ghostty.enable (lib.mkForce (
+      if config.globals.platform == "darwin"
+      then "${config.features.ghostty.package}/Applications/Ghostty.app/Contents/MacOS/ghostty"
+      else "${config.features.ghostty.package}/bin/ghostty"
+    ));
+  };
+
+  home = { config, ... }:
+    let
+      mono = config.features.font.fonts.monospace;
+      scheme = config.features.theme.scheme;
+    in
+    {
+      programs.ghostty = {
+        enable = true;
+        package = config.features.ghostty.package;
+
+        settings = {
+          theme = "dotty";
+
+          window-padding-x = 8;
+          window-padding-y = 8;
+
+          font-family = mono.name;
+          font-style = "Regular";
+
+          font-family-bold = mono.name;
+          font-style-bold = "Bold";
+
+          font-family-italic = mono.name;
+          font-style-italic = "Italic";
+
+          font-family-bold-italic = mono.name;
+          font-style-bold-italic = "Bold Italic";
+
+          font-size = mono.size;
+        };
+
+        themes.dotty = with scheme; {
+          background = "#${base00}";
+          cursor-color = "#${base05}";
+          foreground = "#${base05}";
+          palette = [
+            "0=#${base05}"
+            "1=#${base08}"
+            "2=#${base0B}"
+            "3=#${base0A}"
+            "4=#${base0D}"
+            "5=#${base0E}"
+            "6=#${base0C}"
+            "7=#${base05}"
+            "8=#${base03}"
+            "9=#${base08}"
+            "10=#${base0B}"
+            "11=#${base0A}"
+            "12=#${base0D}"
+            "13=#${base0E}"
+            "14=#${base0C}"
+            "15=#${base06}"
+          ];
+          selection-background = "#${base02}";
+          selection-foreground = "#${base05}";
+        };
+      };
+    };
+}
