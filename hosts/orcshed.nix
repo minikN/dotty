431d0b4e9f7c6a9f7501ecd4d25dd31956afbf25
Demis Balbach <db@minikn.xyz>
2026-06-29 23:13:33 +0200

    hosts: add orcshed config

---
+++ hosts/orcshed.nix
@@ -0,0 +1,26 @@
+{
+  system = "x86_64-linux";
+
+  features = {};
+
+  nixos = { inputs, pkgs, ... }: {
+    imports = [
+      inputs.nixos-hardware.nixosModules.common-cpu-intel
+      inputs.nixos-hardware.nixosModules.common-pc-ssd
+    ];
+
+    networking.hostName = "orcshed";
+
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_latest;
+    boot.initrd.availableKernelModules = [
+      "xhci_pci"
+      "ahci"
+      "usbhid"
+      "usb_storage"
+      "sd_mod"
+    ];
+
+    hardware.enableRedistributableFirmware = true;
+  };
+}
