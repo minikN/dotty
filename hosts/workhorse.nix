9c7f5b4ce685b325e22088abeacf333e6791419f
Demis Balbach <db@minikn.xyz>
2026-07-08 00:26:45 +0200

    hosts: workhorse: enable aerospace; choose-gui; password-store; disable keychain

--- hosts/workhorse.nix
+++ hosts/workhorse.nix
@@ -4,10 +4,14 @@
   features = {
     font.enable = true;
     theme.enable = true;
+    aerospace.enable = true;
+    choose-gui.enable = true;
     ghostty.enable = true;
     ssh.enable = true;
+    password-store.enable = true;
     gnupg = {
       enable = true;
+      keychainInteraction = false;
       sshKeys = [ "E3FFA5A1B444A4F099E594758008C1D8845EC7C0" ];
     };
     zsh = {
